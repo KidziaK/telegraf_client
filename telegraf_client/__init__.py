a1753e4595ef766eae78414af1fb6b5def3e5336
mikolajkida <mikolaj.kida@daftcode.pl>
2025-10-10 15:33:44 +0200

    Bump version to 0.1.4

--- telegraf_client/__init__.py
+++ telegraf_client/__init__.py
@@ -1,3 +1,3 @@
-from ._telegraf_client import Client, Point, TelegrafBindingError
+from .telegraf_client import Client, Point, TelegrafBindingError
 
 __all__ = ["Client", "Point", "TelegrafBindingError"]
